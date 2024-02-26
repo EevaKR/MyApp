85d89f85a19081f0e558de0a22db605ebfbb2410
eeva <t2roee00@students.oamk.fi>
2024-02-26 11:07:36 +0200

    first commit

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Button, Text, TextInput, SafeAreaView, ScrollView } from 'react-native'
 import { orderBy, app, collection, addDoc, MESSAGES, serverTimestamp, query, onSnapshot, firestore } from './firebase/Config';
 import React, { useState, useEffect } from 'react';
+import { convertFirebaseTimeStampToJS } from './helpers/Functions';
 
 
 export default function App() {
@@ -15,7 +16,12 @@ export default function App() {
       const tempMessages = []
 
       querySnapshot.forEach((doc) => {
-        tempMessages.push(doc.data())
+        const messageObject = {
+          id: doc.id,
+          text: doc.data().text,
+          created: convertFirebaseTimeStampToJS(doc.data().created)
+        }
+        tempMessages.push(messageObject)
       })
       setMessages(tempMessages)
     })
@@ -40,7 +46,8 @@ export default function App() {
       <ScrollView>
         {
           messages.map((message) => (
-            <View style={styles.message}>
+            <View style={styles.message} key ={message.id}>
+              <Text style={styles.messageInfo}>{message.created}</Text>
               <Text>{message.text}</Text>
             </View>
           ))
@@ -55,7 +62,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#7fb366',
-    alignItems: 'center',
+    alignItems: 'left',
     justifyContent: 'center',
   },
   message: {
@@ -68,7 +75,8 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     marginLeft: 10,
     marginRight: 10
-
-
+  },
+  messageInfo: {
+    fontSize: 12
   }
 });
